dc471dee4f3950f58007666064475664b4cbb8b7
Stephanie Lima <slrocha17@gmail.com>
2020-06-27 19:24:46 -0300

    Troca de rota em arquivo web para api

--- resources/js/components/contasCorrentes/index.vue
+++ resources/js/components/contasCorrentes/index.vue
@@ -2,17 +2,21 @@
     <table class="table table-bordered">
         <thead>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
-            <th>Ações</th>
+            <th>Agência</th>
+            <th>Conta</th>
+            <th>Dígito</th>
+            <th>Titular</th>
+            <th>CPF</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
+            <td>8585</td>
+            <td>457896</td>
+            <td>3</td>
+            <td>Maria de Lourdes da Cruz</td>
+            <td>047.474.784-45</td>
             <td>
                 <button type="button" class="btn btn-primary">Ver</button>
                 <button type="button" class="btn btn-warning">Editar</button>
